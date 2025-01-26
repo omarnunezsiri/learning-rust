eec7c76d289c64a05e7adbf985de23d44075d8f8
Omar N <menesiena@proton.me>
2025-01-25 20:47:40 -0500

    feat(basics): add first rust project with learnings

---
+++ my_first_project/src/main.rs
@@ -0,0 +1,21 @@
+// how-to build and run:
+// rustc main.rs ---> like gcc main.c
+//      ./main
+// or cargo run
+// or cargo run main.rs
+// or cargo build ---> like make
+//     ./target/debug/my_first_project
+// or cargo build --release
+//     ./target/release/my_first_project
+
+// auto format
+// rustfmt main.rs
+// cargo fmt
+
+// auto check
+// cargo check
+
+fn main() // main function
+{
+    println!("Hello, world!"); // display to console
+}
